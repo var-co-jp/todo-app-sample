d4a522bf8536ee77f4fcc663596d692980c0a2a7
Junichi Nishi <github@purewest.dip.jp>
2022-12-08 16:58:26 +0900

    ログイン機能を修正

--- flask_todo/models.py
+++ flask_todo/models.py
@@ -1,19 +1,26 @@
 from flask_todo import db, login_manager
 from flask_bcrypt import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from sqlalchemy.sql import func
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
 
+
+# Userテーブルの定義
 class User(UserMixin, db.Model):
+    # テーブル名
     __tablename__ = 'users'
     
+    # カラム定義
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), index=True)
     password = db.Column(db.String(128))
-    
+    created_at = db.Column(db.DateTime, server_default=func.now(), nullable=False)
+
+    tasks = db.relationship("Task", backref="users")
     
     def __init__(self, email, username, password):
         self.email = email
@@ -22,12 +29,7 @@ class User(UserMixin, db.Model):
         
     def validate_password(self, password):
         return check_password_hash(self.password, password)
-    
-    def add_user(self):
-        with db.session.begin(subtransactions=True):
-            db.session.add(self)
-        db.session.commit()
-    
+            
     @classmethod
     def select_by_email(cls, email):
         return cls.query.filter_by(email=email).first()
--- flask_todo/views.py
+++ flask_todo/views.py
@@ -1,51 +1,106 @@
-from flask import Blueprint, request, render_template, redirect, url_for
-from flask_login import login_user, login_required, logout_user
-from flask_todo.forms import LoginForm, RegisterForm
+from flask import Blueprint, request, render_template, redirect, url_for, flash
+from flask_login import login_user, login_required, logout_user, current_user
 from flask_todo.models import User
+from datetime import datetime, date
+from flask_todo import db
+import re
 
 bp = Blueprint('todo_app', __name__, url_prefix='')
 
+
+# 初期画面はhome.htmlを表示させます
 @bp.route('/')
 def home():
     return render_template('home.html')
 
-@bp.route('/welcome')
-@login_required
-def welcome():
-    username = User.username
-    return render_template('welcome.html', username=username)
 
+# ログアウトするとhome.htmlに戻る
 @bp.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('todo_app.home'))
 
+
+# ログイン
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm(request.form)
-    if request.method == 'POST' and form.validate():
-        user = User.select_by_email(form.email.data)
-        if user and user.validate_password(form.password.data):
+    # 書き込まれた項目を取得する
+    email = request.form.get('email')
+    password = request.form.get('password')
+    # POSTリクエストの場合
+    if request.method == 'POST':
+        user = User.select_by_email(email)
+        # メールアドレスとパスワードが正しい場合
+        if user and user.validate_password(password):
             login_user(user)
             next = request.args.get('next')
             if not next:
-                next = url_for('todo_app.welcome')
+                next = url_for('todo_app.user')
             return redirect(next)
-    return render_template('login.html', form=form)
+    return render_template('login.html', last_access=datetime.now())
 
+
+# ユーザー登録
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegisterForm(request.form)
-    if request.method == 'POST' and form.validate():
-        user = User(
-            email = form.email.data,
-            username = form.username.data,
-            password = form.password.data
-        )
-        user.add_user()
-        return redirect(url_for('todo_app.login'))
-    return render_template('register.html', form=form)
+    # 書き込まれた項目を取得する
+    username = request.form.get('name')
+    email = request.form.get('email')
+    password1 = request.form.get('password1')
+    password2 = request.form.get('password2')
+    
+    # メールアドレスの形式を正規表現で想定している
+    pattern = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+    
+    # POSTリクエストの場合
+    if request.method == 'POST':
+        # どこかに空欄がある場合
+        if username == '' or email =='' or password1 == '' or password2 == '':
+            flash('空のフォームがあります')
+        # パスワードが一致しない場合
+        elif password1 != password2:
+            flash('パスワードが一致しません')
+        # メールアドレスの形式になっていない場合
+        elif re.match(pattern, email) is None:
+            flash('メールアドレスの形式になっていません')
+        # 全て正しく入力された場合
+        else:
+            # 書き込まれた項目を取得する
+            user = User(
+                email = email,
+                username = username,
+                password = password1
+                )
+            # データベースにあるメールアドレスを取得する
+            # データベースにメールアドレスが登録されていなければNoneとなる
+            DBuser = User.select_by_email(email)
+            
+            # メールアドレスが取得された場合
+            if DBuser != None:
+                flash('登録済みです')
+            # メールアドレスが取得されなかった場合
+            else:
+                # データベースへの書き込みを行う
+                try:
+                    # データベースとの接続を開始する
+                    with db.session.begin(subtransactions=True):
+                        # データベースに書き込むデータを用意する
+                        db.session.add(user)
+                    # データベースへの書き込みを実行する
+                    db.session.commit()
+                # 書き込みがうまくいかない場合
+                except:
+                    # データベースへの書き込みを行わずにロールバックする
+                    db.session.rollback()
+                    raise
+                # データベースとの接続を終了する
+                finally:
+                    db.session.close()
+                    
+                # 成功すればlogin.htmlに遷移する
+                return redirect(url_for('todo_app.login'))
+    return render_template('register.html')
 
 @bp.route('/user')
 @login_required
